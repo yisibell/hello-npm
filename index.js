4f0785cad06ed1bee1d7156e1f9fd2fbc0bd9769
hongwenqing <yisiwings@163.com>
2019-09-05 13:54:31 +0800

    update: test

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ module.exports = function ( str ){
   const version = '1.1.0'
   console.log( 'hello' + str + '!' )
 }
+
+
+// class hScript extends HTMLElement {
+//   // ...
+// }
+
+// window.customElements.define( 'h-script' , hScript )
