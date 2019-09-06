eee9d84c9a2f7628138a12d9afa8e86073554efc
hongwenqing <yisiwings@163.com>
2019-09-06 15:43:57 +0800

    fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // log
 module.exports = function ( str ){
-  const version = '1.2.0'
+  const version = '1.2.1'
   console.log( 'hello' + str + '!' )
 }
 
 // npm version from-git v1.1.2
 // npm version from-git v1.1.2-alpha.0
 // npm version from-git v1.2.0
+// bug fix to v1.2.1
