a66466b915e672fcd7d8df4301b8c362ea988980
hongwenqing <yisiwings@163.com>
2019-09-06 15:03:08 +0800

    v2.0.4

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ module.exports = function ( str ){
 // npm version prepatch v2.0.2-0
 // npm version prepatch v2.0.3-0
 // npm version prerelease v2.0.3-1
+// npm version prerelease --preid=alpha v2.0.3-alpha.0
+// npm version patch v2.0.4
